e61b7b643f94d22df4c24962d460559c3f28c603
Wivel <wiveloriginal@yandex.ru>
2018-10-24 20:29:13 +0300

    3 point

--- scripts.js
+++ scripts.js
@@ -1,21 +1,12 @@
 
-let Timmer_ID;
-function DoIt() {
-    let elem = document.getElementById('display');
-   elem.innerHTML = parseInt(elem.innerHTML)+1;
-}
-function Timer() {
-    Timmer_ID       = window.setInterval(DoIt,1000);
-    let elem        = document.getElementById('button'),
-    elem_stop       = document.getElementById('Stop_button');
-    elem.disabled       = true;
-    elem_stop.disabled  = false;
+function Clock() {
+    let
+        elem            = document.getElementById('display'),
+        now             = new Date();
+        elem.innerHTML  = now.getHours()+':'+now.getMinutes()+':'+now.getSeconds();
+        Clock();
 }
 
-function StopTimer() {
-    window.clearInterval(Timmer_ID);
-    let elem        = document.getElementById('button'),
-        elem_stop       = document.getElementById('Stop_button');
-    elem.disabled       = false;
-    elem_stop.disabled  = true;
+function Timer() {
+    window.setInterval(Clock,1000);
 }
